c179e819bc87bc7d7f21578d23dce372d2755363
Lorys Jelezian <lorysjele@gmail.com>
2022-03-26 19:06:45 +0100

    class to component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,69 @@
 import "./App.css";
-import { Header} from "./models/Header";
-import { Cocktails} from "./models/Cocktails";
-import React from 'react';
+import { Header } from "./models/Header";
+import React, { useEffect } from "react";
+import { Card, Grid } from "@mui/material";
+import "./models/CustomCard.css";
 
-function App(){
-  const [value, setValue] = React.useState('');
-  
+function App() {
+  const [value, setValue] = React.useState("");
+
   return (
-  <div className="app">
-    <Header value={value} setValue={setValue}></Header>
-    <Cocktails name={value}></Cocktails>
-  </div>
-  )
+    <div className="app">
+      <Header value={value} setValue={setValue}></Header>
+      <Cocktails name={value}></Cocktails>
+    </div>
+  );
 }
 
+function Cocktails(props) {
+  const [items, setItems] = React.useState("");
+  const [detailsLoaded, setDetailsLoaded] = React.useState(false);
+  const {name} = props;
+
+  const FetchFromUriAsync = async() => {
+
+    setDetailsLoaded(false);
+
+    try{
+      const resp = await fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Ordinary_Drink")
+      .then(r => r.json());
+      console.log(resp)
+      setItems(resp)
+      setDetailsLoaded(true)
+    } catch(e){
+      setDetailsLoaded(false)
+    }
+  }
+
+  useEffect(()=>{
+    FetchFromUriAsync();
+  })
+
+
+  console.log(detailsLoaded)
+  if (!detailsLoaded)
+    return (
+      <div className="loader">
+        <iframe src="https://embed.lottiefiles.com/animation/8438"></iframe>
+      </div>
+    );
+
+  return (
+    <div className="cocktails">
+      <Grid container className="grid" spacing={5}>
+        {items.drinks.map((drink) => (
+          <Grid item>
+            <Card className="card">
+              <img src={drink.strDrinkThumb} alt={drink.strDrinkThumb}></img>
+              <div className="drink-info">
+                <p className="drink-name">{drink.strDrink}</p>
+              </div>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </div>
+  );
+}
 
 export default App;
--- src/models/Cocktails.js
+++ src/models/Cocktails.js
@@ -1,75 +1,75 @@
-import React from "react";
-import { Card, Grid } from "@mui/material";
-import "./CustomCard.css";
+// import React from "react";
+// import { Card, Grid } from "@mui/material";
+// import "./CustomCard.css";
 
-export class Cocktails extends React.Component {
-  constructor(props) {
-    super(props);
+// export class Cocktails extends React.Component {
+//   constructor(props) {
+//     super(props);
 
-    this.state = {
-      items: [],
-      DataisLoaded: false,
-    };
-  }
+//     this.state = {
+//       items: [],
+//       DataisLoaded: false,
+//     };
+//   }
 
-  componentDidMount(uri) {
-    if(uri === "" || uri == null){
-      uri = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Ordinary_Drink";
-    }
-    console.log(uri);
-    fetch(
-      uri
-    )
-      .then((res) => res.json())
-      .then((json) => {
-        if(json.drinks != null){
-          this.setState({
-            items: json,
-            DataisLoaded: true,
-          });
-        }
-      });
-  }
+//   componentDidMount(uri) {
+//     if(uri === "" || uri == null){
+//       uri = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Ordinary_Drink";
+//     }
+//     console.log(uri);
+//     fetch(
+//       uri
+//     )
+//       .then((res) => res.json())
+//       .then((json) => {
+//         if(json.drinks != null){
+//           this.setState({
+//             items: json,
+//             DataisLoaded: true,
+//           });
+//         }
+//       });
+//   }
 
-  render() {
-    let uri = "";
-    const searchValue = this.props;
-    const { DataisLoaded, items } = this.state;
+//   render() {
+//     let uri = "";
+//     const searchValue = this.props;
+//     const { DataisLoaded, items } = this.state;
 
-    if(searchValue.name.length > 0){
-      console.log(searchValue.name)
+//     if(searchValue.name.length > 0){
+//       console.log(searchValue.name)
 
-      uri = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + JSON.stringify(searchValue.name).replace('"', '').slice(0, -1);
-      this.componentDidMount(uri)
-    } else {
-      uri = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Ordinary_Drink";
-    }
+//       uri = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + JSON.stringify(searchValue.name).replace('"', '').slice(0, -1);
+//       this.componentDidMount(uri)
+//     } else {
+//       uri = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Ordinary_Drink";
+//     }
 
-    if (!DataisLoaded)
-      return (
-        <div className="loader">
-          <iframe src="https://embed.lottiefiles.com/animation/8438"></iframe>
-        </div>
-      );
+//     if (!DataisLoaded)
+//       return (
+//         <div className="loader">
+//           <iframe src="https://embed.lottiefiles.com/animation/8438"></iframe>
+//         </div>
+//       );
 
-    return (
-      <div className="cocktails">
-        <Grid container className="grid" spacing={5}>
-          {items.drinks.map((drink) => (
-              <Grid item>
-                  <Card className="card">
-                    <img
-                      src={drink.strDrinkThumb}
-                      alt={drink.strDrinkThumb}
-                    ></img>
-                    <div className="drink-info">
-                        <p className="drink-name">{drink.strDrink}</p>
-                    </div>
-                  </Card>
-              </Grid>
-          ))}
-        </Grid>
-      </div>
-    );
-  }
-}
+//     return (
+//       <div className="cocktails">
+//         <Grid container className="grid" spacing={5}>
+//           {items.drinks.map((drink) => (
+//               <Grid item>
+//                   <Card className="card">
+//                     <img
+//                       src={drink.strDrinkThumb}
+//                       alt={drink.strDrinkThumb}
+//                     ></img>
+//                     <div className="drink-info">
+//                         <p className="drink-name">{drink.strDrink}</p>
+//                     </div>
+//                   </Card>
+//               </Grid>
+//           ))}
+//         </Grid>
+//       </div>
+//     );
+//   }
+// }
